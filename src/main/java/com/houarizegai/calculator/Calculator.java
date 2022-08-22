3cad987873a6d1f27b8fa809bb1aa8400028c967
Rajashekarg12 <110624878+Rajashekarg12@users.noreply.github.com>
2022-08-22 16:50:06 +0530

    Update Calculator.java

--- src/main/java/com/houarizegai/calculator/Calculator.java
+++ src/main/java/com/houarizegai/calculator/Calculator.java
@@ -12,17 +12,13 @@ import java.lang.Math;
 
 public class Calculator {
 
-    String name = null;
-    String id =null;
-    String text=null;
-    int id =0;
+    
     private static final int WINDOW_WIDTH = 410;
     private static final int WINDOW_HEIGHT = 600;
     private static final int BUTTON_WIDTH = 80;
     private static final int BUTTON_HEIGHT = 70;
     private static final int MARGIN_X = 20;
     private static final int MARGIN_Y = 60;
-//HI
     
     private JFrame window; // Main window
     private JComboBox<String> comboCalcType, comboTheme;
