0b499c8358114ee5e5afdf9edfa4957eaf304c80
Rajashekarg12 <110624878+Rajashekarg12@users.noreply.github.com>
2022-08-22 13:50:33 +0530

    Update Calculator.java

--- src/main/java/com/houarizegai/calculator/Calculator.java
+++ src/main/java/com/houarizegai/calculator/Calculator.java
@@ -13,6 +13,7 @@ import java.lang.Math;
 public class Calculator {
 
     String name = null;
+    String id =null;
     private static final int WINDOW_WIDTH = 410;
     private static final int WINDOW_HEIGHT = 600;
     private static final int BUTTON_WIDTH = 80;
